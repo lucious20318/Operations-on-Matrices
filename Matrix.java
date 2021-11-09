aa1e5cf7d2c45b38eeb9824b1d3975f41e2217b3
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-11-09 10:10:55 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -189,3 +189,73 @@ public class Skew extends Symmetric
         }
     }
 }
+
+public class Upper extends Square
+{
+    @Override 
+    public void checker(String id)
+    {
+        ArrayList<ArrayList<Integer>> x = new ArrayList<>();
+        ArrayList<Integer> z = new ArrayList<>();
+
+        x = getter_mat(id);
+        
+        z = getter_ord(id);
+
+        int order = z.get(0);
+
+        int flag = 0;
+
+        for(int i=1; i<order; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(x.get(i).get(j) != 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+
+        if(flag == 0)
+        {
+            System.out.println(id + "is a upper triangular matrix");
+        }
+    }
+}
+
+public class Lower extends Square
+{
+    @Override
+    public void checker(String id)
+    {
+        ArrayList<ArrayList<Integer>> x = new ArrayList<>();
+        ArrayList<Integer> z = new ArrayList<>();
+
+        x = getter_mat(id);
+        
+        z = getter_ord(id);
+
+        int order = z.get(0);
+
+        int flag = 0;
+
+        for(int i=0; i<order; i++)
+        {
+            for(int j=i+1; j<order; j++)
+            {
+                if(x.get(i).get(j) != 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+
+        if(flag == 0)
+        {
+            System.out.println(id + "is a lower triangular matrix");
+        }        
+    }
+}
