daf45d63c92b009d3b6051449460b40a72b30b71
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-11-09 09:49:01 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -97,7 +97,7 @@ public class Symmetric extends Square
         
         z = getter_ord(id);
 
-        order = z.get(0);
+        int order = z.get(0);
 
         ArrayList<ArrayList<Integer>> y = new ArrayList<>();
         y = transpose(x,order);
@@ -114,6 +114,7 @@ public class Symmetric extends Square
                 if(e1!=e2)
                 {
                     flag = 1;
+                    break;
                 }
             }
         }
@@ -142,5 +143,49 @@ public class Symmetric extends Square
         }
 
         return y;
+    }
+}
+
+public class Skew extends Symmetric
+{
+    @Override
+    public void checker(String id)
+    {
+        ArrayList<ArrayList<Integer>> x = new ArrayList<>();
+        ArrayList<Integer> z = new ArrayList<>();
+
+        x = getter_mat(id);
+        
+        z = getter_ord(id);
+
+        int order = z.get(0);
+
+        ArrayList<ArrayList<Integer>> y = new ArrayList<>();
+
+        y = transpose(x, order);
+
+        int flag = 0;
+
+        for(int i=0; i<order; i++)
+        {
+            for(int j=0; j<order; j++)
+            {
+                int e1 = x.get(i).get(j);
+                int e2 = y.get(i).get(j);
+
+                e2 = e2*(-1);
+
+                if(e1 != e2)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+
+        if (flag == 0)
+        {
+            System.out.println(id + " is a skew - symmetric matrix");
+        }
     }
 }
