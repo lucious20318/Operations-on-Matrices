5319ba62c7c0b5bc8135c0b9d6b9a91a91888f55
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-11-10 11:56:04 +0530

    Update Singular.java

--- Singular.java
+++ Singular.java
@@ -5,8 +5,8 @@ import java.io.*;
 
 public class Singular extends Determinant
 {
-    @Override
-    public void checker(String id)
+    
+    public int check(String id)
     {
         ArrayList<ArrayList<Integer>> x = new ArrayList<>();
         ArrayList<Integer> z = new ArrayList<>();
@@ -27,8 +27,9 @@ public class Singular extends Determinant
         if(result == 0)
         {
             System.out.println(id + " is a singular matrix");
+            return 0;
         }
         
-        return;
+        return 1;
     }
 }
